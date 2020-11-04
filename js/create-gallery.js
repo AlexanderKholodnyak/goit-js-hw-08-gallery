559d9835b1a93ccc5dff44a85f607e1a6ee1e6e0
Alexander Kholodnyak <kholodnyak.a@gmail.com>
2020-11-04 23:35:00 +0200

    finish

--- js/create-gallery.js
+++ js/create-gallery.js
@@ -1,18 +1,22 @@
 import galleryItems from './gallery-items.js';
 
-const galleryContainer = document.querySelector(".js-gallery");
-const modal = document.querySelector(".js-lightbox");
-const modalImg = document.querySelector(".lightbox__image");
-const modalContent = document.querySelector(".lightbox__image");
-const overlay = document.querySelector(".lightbox__overlay")
-const modalBtnClose = document.querySelector(".lightbox__button");
-const modalBtnRight = document.querySelector(".scroll-right");
-const modalBtnLeft = document.querySelector(".scroll-left");
 
-galleryContainer.addEventListener('click', modalOpen);
-galleryContainer.insertAdjacentHTML("beforeend", galleryCardMarkup(galleryItems));
+const refs = {
+    galleryContainer: document.querySelector(".js-gallery"),
+    modal: document.querySelector(".js-lightbox"),
+    modalImg:document.querySelector(".lightbox__image"),
+    modalContent: document.querySelector(".lightbox__image"),
+    overlay: document.querySelector(".lightbox__overlay"),
+    modalBtnClose: document.querySelector(".lightbox__button"),
+    modalBtnRight: document.querySelector(".scroll-right"),
+    modalBtnLeft: document.querySelector(".scroll-left"),
+}
+
+
+
+refs.galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup(galleryItems));
    
-function galleryCardMarkup(img) {
+function galleryMarkup(img) {
     return img.map(({ preview, original, description }) => {
         return `<li class="gallery__item">
                     <a class="gallery__link"
@@ -26,33 +30,36 @@ function galleryCardMarkup(img) {
     }).join("");
 };
 
+refs.galleryContainer.addEventListener('click', modalOpen);
+
 function modalOpen(event) {
     event.preventDefault();
 
     if (event.target.nodeName !== "IMG") {
         return
     };
-    modal.classList.add("is-open");
-    modalImg.src = event.target.dataset.source;
-    modalImg.alt = event.target.alt;
-    overlay.addEventListener("click", modalCloseByOverlayClick);
+    refs.modal.classList.add("is-open");
+    refs.modalImg.src = event.target.dataset.source;
+    refs.modalImg.alt = event.target.alt;
+    refs.overlay.addEventListener("click", modalCloseByOverlayClick);
     document.addEventListener("keydown", modalCloseByEsc);
-    modalBtnClose.addEventListener('click', modalClose);
-     window.addEventListener("keydown", modalImgScrolling);
-    modalBtnRight.addEventListener("click", modalImgScrolling);
-    modalBtnLeft.addEventListener("click", modalImgScrolling);
-    modalContent.addEventListener("click", modalImgScrolling);   
+    refs.modalBtnClose.addEventListener('click', modalClose);
+    window.addEventListener("keydown", modalImgScrolling);
+    refs.modalBtnRight.addEventListener("click", modalImgScrolling);
+    refs.modalBtnLeft.addEventListener("click", modalImgScrolling);
+    refs.modalContent.addEventListener("click", modalImgScrolling);   
 };
 
+
 function modalClose(event) {
-    modal.classList.remove("is-open");
-    overlay.removeEventListener("click", modalCloseByOverlayClick);
+    refs.modal.classList.remove("is-open");
+    refs.overlay.removeEventListener("click", modalCloseByOverlayClick);
     document.removeEventListener("keydown", modalCloseByEsc);
-    modalBtnClose.removeEventListener('click', modalClose);
+    refs.modalBtnClose.removeEventListener('click', modalClose);
     window.removeEventListener("keydown", modalImgScrolling);
-    modalBtnRight.removeEventListener("click", modalImgScrolling);
-    modalBtnLeft.removeEventListener("click", modalImgScrolling);
-    modalContent.removeEventListener("click", modalImgScrolling);
+    refs.modalBtnRight.removeEventListener("click", modalImgScrolling);
+    refs.modalBtnLeft.removeEventListener("click", modalImgScrolling);
+    refs.modalContent.removeEventListener("click", modalImgScrolling);
 };
 
 function modalCloseByEsc(event) {
@@ -70,23 +77,24 @@ function modalCloseByOverlayClick(event) {
 
 function modalImgScrolling(event) {
 
-    let imgIndex = galleryItems.findIndex(img => img.original === modalImg.src);
+    
+    let imgIndex = galleryItems.findIndex(img => img.original === refs.modalImg.src);
 
-    if (event.code === 'ArrowLeft' || event.code === 'ArrowDown' || modalBtnLeft === event.target) {
+    if (event.code === 'ArrowLeft' || event.code === 'ArrowDown' || refs.modalBtnLeft === event.target) {
         if (imgIndex === 0) {
             imgIndex += galleryItems.length;
         }
         imgIndex -= 1;
     };
 
-    if (event.code === 'ArrowRight' || event.code === 'ArrowUp' || modalBtnRight === event.target || modalContent === event.target) {
+    if (event.code === 'ArrowRight' || event.code === 'ArrowUp' || refs.modalBtnRight === event.target || refs.modalContent === event.target) {
         if (imgIndex === galleryItems.length - 1) {
             imgIndex -= galleryItems.length;
         }
         imgIndex += 1;
     };
 
-    modalImg.src = galleryItems[imgIndex].original;
-    modalImg.alt = galleryItems[imgIndex].description;
+    refs.modalImg.src = galleryItems[imgIndex].original;
+    refs.modalImg.alt = galleryItems[imgIndex].description;
 
 };
